280b1aee71fe5fa707f4999476a51f2bbdc79c5c
Fauzi0309 <fauzirahmandhani@gmail.com>
2024-08-30 19:07:26 +0700

    server-side caching implemented

--- src/server.js
+++ src/server.js
@@ -25,10 +25,12 @@ const UploadsValidator = require("./validator/uploads");
 
 // const StorageService = require("./services/storage/StorageService");
 const StorageService = require('./services/S3/StorageService');
+const CacheService = require('./services/redis/CacheService');
 
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
 
